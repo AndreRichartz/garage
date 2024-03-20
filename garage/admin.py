447f16abf7b7859aee214b47c1293c54ef9aae3f
AndreRichartz <andrerichartz07@gmail.com>
2024-03-19 21:20:08 -0300

    vehicle model created

--- garage/admin.py
+++ garage/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from garage.models import Brand, Category, Acessory, Color
+from garage.models import Brand, Category, Acessory, Color, Vehicle
 
 admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(Acessory)
 admin.site.register(Color)
+admin.site.register(Vehicle)
+
--- garage/models.py
+++ garage/models.py
@@ -15,11 +15,19 @@ class Category(models.Model):
     def __str__(self):
         return self.description_cat
     
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
 class Acessory(models.Model):
     description_ace = models.CharField(max_length=100)
 
     def __str__(self):
         return self.description_ace
+    
+    class Meta:
+        verbose_name = "Acessory"
+        verbose_name_plural = "Acessories"
     
 class Color(models.Model):
     description_col = models.CharField(max_length=100)
@@ -27,5 +35,24 @@ class Color(models.Model):
     def __str__(self):
         return self.description_col
     
+    class Meta:
+        verbose_name = "Color"
+        verbose_name_plural = "Colors"
+    
+class Vehicle(models.Model):
+    brand = models.ForeignKey(
+        Brand,  on_delete=models.PROTECT, related_name = "vehicles"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, related_name = "vehicles"
+    )
+    color = models.ForeignKey(
+        Color, on_delete=models.PROTECT, related_name="vehicles"
+    )
+    year = models.IntegerField(default=0, null=True)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2,null=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.category} {self.year} {self.color}"
     
     
